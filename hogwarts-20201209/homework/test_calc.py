fe5be338efa9c2bdd16d603709c6074362c0b39c
wushenghang <wushenghang@lizhi.fm>
2020-12-10 00:50:43 +0800

    完成作业

---
+++ hogwarts-20201209/homework/test_calc.py
@@ -0,0 +1,55 @@
+import pytest
+from pythoncode.calculator import Calculator
+
+
+class TestCalc:
+    def setup_method(self):
+        self.calc = Calculator()
+        print("开始计算")
+
+    def teardown_method(self):
+        print("结束计算")
+
+    @pytest.mark.parametrize("a", [-100, -1, 0, 1, 100])
+    @pytest.mark.parametrize("b", [-100, -1, 0, 1, 100])
+    def test_add(self, a, b):
+        res = self.calc.add(a, b)
+        real = a + b
+        print(f"计算: {a} + {b}")
+        print(f"程序结果为：{res}")
+        print(f"实际结果为：{real}")
+        assert res == real
+
+    @pytest.mark.parametrize("a", [-100, -1, 0, 1, 100])
+    @pytest.mark.parametrize("b", [-100, -1, 0, 1, 100])
+    def test_sub(self, a, b):
+        res = self.calc.sub(a, b)
+        real = a - b
+        print(f"计算: {a} - {b}")
+        print(f"程序结果为：{res}")
+        print(f"实际结果为：{real}")
+        assert res == real
+
+    @pytest.mark.parametrize("a", [-100, -1, 0, 1, 100])
+    @pytest.mark.parametrize("b", [-100, -1, 0, 1, 100])
+    def test_mul(self, a, b):
+        res = self.calc.mul(a, b)
+        real = a * b
+        print(f"计算: {a} * {b}")
+        print(f"程序结果为：{res}")
+        print(f"实际结果为：{real}")
+        assert res == real
+
+    @pytest.mark.parametrize("a", [-100, -1, 0, 1, 100])
+    @pytest.mark.parametrize("b", [-100, -1, 1, 100])
+    def test_div(self, a, b):
+        res = self.calc.div(a, b)
+        real = a / b
+        print(f"计算: {a} / {b}")
+        print(f"程序结果为：{res}")
+        print(f"实际结果为：{real}")
+        assert res == real
+
+
+if __name__ == '__main__':
+    pytest.main(["-sv", "hogwarts-20201209/homework/test_calc.py"])
